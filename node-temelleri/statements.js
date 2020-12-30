cee1b8f18ee6c125cf3a735653c7e0e3a09eb61a
SARIKAYA <mustafasarikaya1@hotmail.com>
2020-12-30 12:14:05 +0300

    node js temelleri

---
+++ node-temelleri/statements.js
@@ -0,0 +1,10 @@
+var age = 25;
+if (age > 25) {
+    console.log('you are soldier')
+}
+
+else{
+
+    console.log('you are children')
+
+}
---
+++ node-temelleri/variables.js
@@ -0,0 +1,7 @@
+console.log('hello world')
+var variable_name = 'mustafa';
+var person_name = 'sarikaya';
+console.log(variable_name + " " + person_name );
+var age = 24;
+var height = 15.5;
+console.log(age,height);
